ac26a800dfbe45011b64fb08b71c3960c337866a
sholomabrahams <sholomabrahams@icloud.com>
2020-05-17 00:20:33 -0400

    Changed Database Connection

--- src/spring2020/mcon364/emailScraper/Scraper.java
+++ src/spring2020/mcon364/emailScraper/Scraper.java
@@ -11,22 +11,20 @@ public class Scraper {
     Connection connection;
 
     public Scraper() {
-        connectToDatabase();
-    }
-
-    private void connectToDatabase() {
         try {
-            FileInputStream in = new FileInputStream("src/secrets.txt");
-            Properties props = new Properties();
-            props.load(in);
-            connection = DriverManager.getConnection(
-                    props.getProperty("url") +
-                    ";databaseName=" + props.getProperty("database") +
-                    ";user=" + props.getProperty("username") +
-                    ";password=" + props.getProperty("password")
-            );
+            connection = DriverManager.getConnection(connectionString());
         } catch (SQLException | IOException throwables) {
             throwables.printStackTrace();
         }
+    }
+
+    private String connectionString() throws IOException {
+        FileInputStream in = new FileInputStream("src/secrets.txt");
+        Properties props = new Properties();
+        props.load(in);
+        return props.getProperty("url") +
+                ";databaseName=" + props.getProperty("database") +
+                ";user=" + props.getProperty("username") +
+                ";password=" + props.getProperty("password");
     }
 }
