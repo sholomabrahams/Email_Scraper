7d97f19e8c31af31b1bcb015446c852dfdf7c869
sholomabrahams <sholomabrahams@icloud.com>
2020-05-18 20:08:30 -0400

    Loading Pages is Working

--- src/spring2020/mcon364/emailScraper/Main.java
+++ src/spring2020/mcon364/emailScraper/Main.java
@@ -1,14 +1,7 @@
 package spring2020.mcon364.emailScraper;
 
-import java.io.IOException;
-import java.sql.SQLException;
-
 public class Main {
     public static void main(String[] args) {
-        try {
-            new Scraper();
-        } catch (IOException | SQLException e) {
-            e.printStackTrace();
-        }
+        new Scraper();
     }
 }
--- src/spring2020/mcon364/emailScraper/Scraper.java
+++ src/spring2020/mcon364/emailScraper/Scraper.java
@@ -2,15 +2,16 @@ package spring2020.mcon364.emailScraper;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -24,20 +25,20 @@ public class Scraper {
     private LinkedBlockingQueue<String> urls;
     private final int MAX_EMAILS = 5;
 
-    public Scraper() throws IOException, SQLException {
+    public Scraper() {
         final int MAX_THREADS = 75;
 
-        connection = DriverManager.getConnection(connectionString());
+        //connection = DriverManager.getConnection(connectionString());
         ExecutorService pool = Executors.newFixedThreadPool(MAX_THREADS);
 
-        urls = new LinkedBlockingQueue<>((int) (MAX_EMAILS * 1.25));
-        urls.add("https://www.touro.edu/");
+        urls = new LinkedBlockingQueue<>();
+        urls.add("https://touro.edu/");
         visited = new HashSet<>();
         emails = Collections.synchronizedSet(new HashSet<>(MAX_EMAILS));
         while (emails.size() < MAX_EMAILS && !urls.isEmpty()) {
             pool.execute(new ScraperWorker(urls.poll()));
         }
-        pool.shutdownNow();
+        pool.shutdown();
     }
 
     private String connectionString() throws IOException {
@@ -66,15 +67,19 @@ public class Scraper {
         @Override
         public void run() {
             try {
-                Document doc = Jsoup.connect("https://touro.edu").get();
+                Document doc = Jsoup.connect(URL).get();
                 Elements links = doc.select("a");
-                links.eachAttr("abs:href").forEach(System.out::println);
+                //links.eachAttr("abs:href").forEach(System.out::println);
                 links.eachAttr("abs:href").forEach(a -> {
-                    System.out.println(a);
                     if (!visited.contains(a)) {
                         urls.add(a);
                     }
                 });
+                /*
+                 * This will return all ul elements with text matching
+                 * with regex pattern "C[+]?" (Character C followed
+                 * by zero or more + sign):
+                 * Elements uls = document.select("ul:matches(C[+]?)");*/
                 Matcher emailMatcher = Pattern.compile("[A-Za-z][A-Za-z.-_%+]{0,63}@" +
                         "(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,62}[A-Za-z0-9])?\\.)" +
                         "{1,8}[A-Za-z]{2,63}").matcher(doc.toString());
@@ -83,10 +88,10 @@ public class Scraper {
                     emails.add(emailMatcher.group());
                 }
                 visited.add(URL);
-                if (emails.size() >= MAX_EMAILS) {
+//                if (emails.size() >= MAX_EMAILS) {
                     upload();
-                }
-            } catch (IOException e) {
+//                }
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
