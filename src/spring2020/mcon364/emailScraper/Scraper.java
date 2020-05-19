4ddbc47b56458404bf8a436f9f00cf5f7a4afe3f
sholomabrahams <sholomabrahams@icloud.com>
2020-05-19 19:48:10 -0400

    First Working Version

--- src/spring2020/mcon364/emailScraper/Scraper.java
+++ src/spring2020/mcon364/emailScraper/Scraper.java
@@ -2,15 +2,10 @@ package spring2020.mcon364.emailScraper;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -19,11 +14,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Scraper {
-    private Connection connection;
-    private Set<String> emails;
-    private HashSet<String> visited;
-    private LinkedBlockingQueue<String> urls;
-    private final int MAX_EMAILS = 5;
+    private final Connection CONNECTION = null;
+    private final Set<String> EMAILS;
+    private final HashSet<String> VISITED;
+    private final LinkedBlockingQueue<String> URLS;
+    private final int MAX_EMAILS = 10;
 
     public Scraper() {
         final int MAX_THREADS = 75;
@@ -31,30 +26,34 @@ public class Scraper {
         //connection = DriverManager.getConnection(connectionString());
         ExecutorService pool = Executors.newFixedThreadPool(MAX_THREADS);
 
-        urls = new LinkedBlockingQueue<>();
-        urls.add("https://touro.edu/");
-        visited = new HashSet<>();
-        emails = Collections.synchronizedSet(new HashSet<>(MAX_EMAILS));
-        while (emails.size() < MAX_EMAILS && !urls.isEmpty()) {
-            pool.execute(new ScraperWorker(urls.poll()));
+        URLS = new LinkedBlockingQueue<>();
+        URLS.add("https://touro.edu/");
+        VISITED = new HashSet<>();
+        EMAILS = Collections.synchronizedSet(new HashSet<>(MAX_EMAILS));
+        while (EMAILS.size() < MAX_EMAILS) {
+            if (URLS.peek() != null) {
+                System.out.println(EMAILS.size());
+                pool.execute(new ScraperWorker(URLS.poll()));
+            }
         }
         pool.shutdown();
+        upload();
     }
 
-    private String connectionString() throws IOException {
-        FileInputStream in = new FileInputStream("src/secrets.txt");
-        Properties props = new Properties();
-        props.load(in);
-        return props.getProperty("url") +
-                ";databaseName=" + props.getProperty("database") +
-                ";user=" + props.getProperty("username") +
-                ";password=" + props.getProperty("password");
-    }
+//    private String connectionString() throws IOException {
+//        FileInputStream in = new FileInputStream("src/secrets.txt");
+//        Properties props = new Properties();
+//        props.load(in);
+//        return props.getProperty("url") +
+//                ";databaseName=" + props.getProperty("database") +
+//                ";user=" + props.getProperty("username") +
+//                ";password=" + props.getProperty("password");
+//    }
 
     private void upload() {
         // TODO: upload data to db
-        System.out.println(emails);
-        System.out.println(visited);
+        System.out.println("Emails:  " + EMAILS);
+        System.out.println("URLs:  " + VISITED);
     }
 
     class ScraperWorker implements Runnable {
@@ -68,11 +67,16 @@ public class Scraper {
         public void run() {
             try {
                 Document doc = Jsoup.connect(URL).get();
-                Elements links = doc.select("a");
-                //links.eachAttr("abs:href").forEach(System.out::println);
-                links.eachAttr("abs:href").forEach(a -> {
-                    if (!visited.contains(a)) {
-                        urls.add(a);
+                doc.select("a").eachAttr("abs:href").forEach(a -> {
+                    if (!VISITED.contains(a) && a.indexOf('@') < 0 && !a.contains("tel:")) {
+                        int i = a.indexOf('#');
+                        if (i >= 0) {
+                            a = a.substring(0, i);
+                        }
+                        if (!a.contains("?") && a.charAt(a.length() - 1) != '/') {
+                            a = a + '/';
+                        }
+                        URLS.add(a);
                     }
                 });
                 /*
@@ -83,16 +87,20 @@ public class Scraper {
                 Matcher emailMatcher = Pattern.compile("[A-Za-z][A-Za-z.-_%+]{0,63}@" +
                         "(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,62}[A-Za-z0-9])?\\.)" +
                         "{1,8}[A-Za-z]{2,63}").matcher(doc.toString());
+                //Matcher emailMatcher = Pattern.compile("").matcher(doc.toString());
                 while (emailMatcher.find()) {
-                    System.out.println(emailMatcher.group());
-                    emails.add(emailMatcher.group());
+                    //System.out.println(emailMatcher.group());
+                    if (EMAILS.size() < MAX_EMAILS) {
+                        EMAILS.add(emailMatcher.group());
+                    }
                 }
-                visited.add(URL);
-//                if (emails.size() >= MAX_EMAILS) {
+                //EMAILS.add("sholom@" + URL);
+                VISITED.add(URL);
+                /*if (EMAILS.size() >= MAX_EMAILS) {
                     upload();
-//                }
+                }*/
             } catch (Exception e) {
-                e.printStackTrace();
+                //e.printStackTrace();
             }
         }
     }
