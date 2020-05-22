bb251e973915f8d2aae4cb1e02110ab20fc382b0
sholomabrahams <sholomabrahams@icloud.com>
2020-05-21 23:54:54 -0400

    Fully Working Version

--- src/spring2020/mcon364/emailScraper/Main.java
+++ src/spring2020/mcon364/emailScraper/Main.java
@@ -1,7 +1,19 @@
 package spring2020.mcon364.emailScraper;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        new Scraper(100, 1500);
+        try {
+//            new Scraper(500, 750, 500);
+            new Scraper(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+        } catch (SQLException e) {
+            System.out.println("Could not connect to Database:");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Could not retrieve database secrets.");
+            e.printStackTrace();
+        }
     }
 }
